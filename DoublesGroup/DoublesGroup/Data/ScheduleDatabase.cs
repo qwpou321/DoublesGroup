a1be9442b5651590eaaecbaaa28078dc017a33a0
qwpou321 <qwpou321@gmail.com>
2021-11-23 23:11:31 +0800

    補上傳檔案

---
+++ DoublesGroup/DoublesGroup/Data/ScheduleDatabase.cs
@@ -0,0 +1,36 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DoublesGroup
+{
+    public class ScheduleDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public ScheduleDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Schedule>().Wait();
+        }
+
+        public Task<List<Schedule>> GetPlayersAsync()
+        {
+            return _database.Table<Schedule>().ToListAsync();
+        }
+
+        public async Task UpdateSchedue(List<Schedule> schedules)
+        {
+            await _database.DeleteAllAsync<Schedule>();
+            await _database.InsertAllAsync(schedules);
+        }
+
+        public Task<int> SaveScheduleAsync(Schedule schedule)
+        {
+            return _database.UpdateAsync(schedule);
+
+        }
+    }
+}
---
+++ DoublesGroup/DoublesGroup/Models/Schedule.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace DoublesGroup
+{
+    public class Schedule
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Players { get; set; }
+        public bool Played { get; set; }
+    }
+}
